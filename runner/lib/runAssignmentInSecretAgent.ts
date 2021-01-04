e0d28aab4989a06ef3b67834e66905c1f931259d
Caleb Clark <calebjclark@gmail.com>
2021-01-04 12:04:52 -0600

    run assignments in runner with secret-agent

---
+++ runner/lib/runAssignmentInSecretAgent.ts
@@ -0,0 +1,59 @@
+import { Agent } from 'secret-agent';
+import IAssignment from '@double-agent/collect-controller/interfaces/IAssignment';
+import ISessionPage from '@double-agent/collect/interfaces/ISessionPage';
+
+let lastPage: ISessionPage;
+
+export default async function runAssignmentInSecretAgent(agent: Agent, assignment: IAssignment) {
+  console.log('--------------------------------------');
+  console.log('STARTING ', assignment.id);
+  try {
+    for (const pages of Object.values(assignment.pagesByPlugin)) {
+      await runPluginPages(agent, assignment, pages);
+    }
+    console.log('[%s.✔] Done', assignment.sessionId);
+  } catch (err) {
+    console.log('[%s.x] Error on %s', assignment.sessionId, lastPage?.url, err);
+    process.exit();
+  }
+  console.log('FINISHED ', assignment.id);
+}
+
+async function runPluginPages(agent: Agent, assignment: IAssignment, pages: ISessionPage[]) {
+  let isFirst = true;
+  let counter = 0;
+  for (const page of pages) {
+    lastPage = page;
+    const step = `[${assignment.sessionId}.${counter}]`;
+    if (page.isRedirect) continue;
+    if (isFirst || page.url !== (await agent.url)) {
+      console.log('%s GOTO -- %s', step, page.url);
+      const resource = await agent.goto(page.url);
+      const statusCode = await resource.response.statusCode;
+      if (statusCode >= 400) {
+        console.log(`${statusCode} ERROR: `, await resource.response.text());
+        console.log(page.url);
+        process.exit();
+      }
+    }
+    isFirst = false;
+    await agent.waitForAllContentLoaded();
+
+    if (page.waitForElementSelector) {
+      console.log('%s waitForElementSelector -- %s', step, page.waitForElementSelector);
+      const element = agent.document.querySelector(page.waitForElementSelector);
+      await agent.waitForElement(element, { waitForVisible: true, timeoutMs: 60e3 });
+    }
+
+    if (page.clickElementSelector) {
+      console.log('%s Wait for clickElementSelector -- %s', step, page.clickElementSelector);
+      const clickable = agent.document.querySelector(page.clickElementSelector);
+      await agent.waitForElement(clickable, { waitForVisible: true });
+      console.log('%s Click -- %s', step, page.clickElementSelector);
+      await agent.click(clickable);
+      await agent.waitForLocation('change');
+      console.log('%s Location Changed -- %s', step, page.url);
+    }
+    counter += 1;
+  }
+}
---
+++ runner/scripts/3-runAssignments.ts
@@ -0,0 +1,16 @@
+import * as Path from 'path';
+import { Agent } from 'secret-agent';
+import IAssignment from '@double-agent/collect-controller/interfaces/IAssignment';
+import runAssignmentInSecretAgent from '../lib/runAssignmentInSecretAgent';
+import forEachAssignment from '../lib/forEachAssignment';
+
+(async function run() {
+  const runAssignment = async (assignment: IAssignment) => {
+    const agent = new Agent();
+    await runAssignmentInSecretAgent(agent, assignment);
+    await agent.close();
+  }
+
+  const config = { userId: 'testing', dataDir: Path.resolve(__dirname, '../data/3-ran-assignments'), concurrency: 1 };
+  await forEachAssignment(config, assignment => runAssignment(assignment));
+})().catch(console.log);
