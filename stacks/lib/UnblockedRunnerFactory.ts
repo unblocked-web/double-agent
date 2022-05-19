f063388dba2bec8a484ed70e507ba757bdf636d3
Blake Byrnes <blakebyrnes@gmail.com>
2022-05-19 13:46:18 -0400

    fix: unblocked alpha2 fix

--- stacks/lib/UnblockedRunnerFactory.ts
+++ stacks/lib/UnblockedRunnerFactory.ts
@@ -8,7 +8,7 @@ import DefaultBrowserEmulator, { IEmulatorOptions } from '@unblocked-web/default
 
 export default class UnblockedRunnerFactory implements IRunnerFactory {
   private pool = new Pool({
-    unblockedPlugins: [DefaultBrowserEmulator, DefaultHumanEmulator],
+    plugins: [DefaultBrowserEmulator, DefaultHumanEmulator],
   });
 
   public runnerId(): string {
